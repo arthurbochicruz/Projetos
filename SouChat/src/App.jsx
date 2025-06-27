5b20111f8b2526045530623b25e061e3dca0676c
arthurbochicruz <173960792+arthurbochicruz@users.noreply.github.com>
2025-06-27 20:53:08 -0300

    Commit SouChat Article
    Fiz algumas Alterações no NavBar .
    E Fiz o article colocando o textos, imagens e Button,
    E estilizando e ajustando.

--- SouChat/src/App.jsx
+++ SouChat/src/App.jsx
@@ -1,9 +1,13 @@
 import './App.css'
 import NavBar from './Components/NavBar/navbar'
+import Article from './Components/Article/Article'
 
 function App(){
     return (
-       <NavBar />
+       <div className='HeartOfTheProject'>
+           <NavBar />
+           <Article />
+       </div>
     )
 }
 
